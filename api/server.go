2cd343843a83f196b8e44d3e651785407178a62e
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2024-02-13 04:05:10 -0300

    removind unecessary endpoints

--- api/server.go
+++ api/server.go
@@ -1,3 +1,5 @@
+// Package main provides the main server implementation for a trivia API.
+// It fetches trivia questions from an external API and serves them over HTTP.
 package main
 
 import (
@@ -6,49 +8,52 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 
 	"github.com/urfave/negroni"
 )
 
-var (
-	categoryMap = map[string]int{
-		"General Knowledge":                     9,
-		"Entertainment: Books":                  10,
-		"Entertainment: Film":                   11,
-		"Entertainment: Music":                  12,
-		"Entertainment: Musicals & Theatres":    13,
-		"Entertainment: Television":             14,
-		"Entertainment: Video Games":            15,
-		"Entertainment: Board Games":            16,
-		"Science & Nature":                      17,
-		"Science: Computers":                    18,
-		"Science: Mathematics":                  19,
-		"Mythology":                             20,
-		"Sports":                                21,
-		"Geography":                             22,
-		"History":                               23,
-		"Politics":                              24,
-		"Art":                                   25,
-		"Celebrities":                           26,
-		"Animals":                               27,
-		"Vehicles":                              28,
-		"Entertainment: Comics":                 29,
-		"Science: Gadgets":                      30,
-		"Entertainment: Japanese Anime & Manga": 31,
-		"Entertainment: Cartoon & Animations":   32,
-	}
+// categoryMap maps category names to their corresponding IDs used in the external API.
+var categoryMap = map[string]int{
+	"General Knowledge":                     9,
+	"Entertainment: Books":                  10,
+	"Entertainment: Film":                   11,
+	"Entertainment: Music":                  12,
+	"Entertainment: Musicals & Theatres":    13,
+	"Entertainment: Television":             14,
+	"Entertainment: Video Games":            15,
+	"Entertainment: Board Games":            16,
+	"Science & Nature":                      17,
+	"Science: Computers":                    18,
+	"Science: Mathematics":                  19,
+	"Mythology":                             20,
+	"Sports":                                21,
+	"Geography":                             22,
+	"History":                               23,
+	"Politics":                              24,
+	"Art":                                   25,
+	"Celebrities":                           26,
+	"Animals":                               27,
+	"Vehicles":                              28,
+	"Entertainment: Comics":                 29,
+	"Science: Gadgets":                      30,
+	"Entertainment: Japanese Anime & Manga": 31,
+	"Entertainment: Cartoon & Animations":   32,
+}
 
-	client = &http.Client{
-		Timeout: 10 * time.Second,
-	}
-)
+// client is the HTTP client used to make API requests.
+var client = &http.Client{
+	Timeout: 10 * time.Second,
+}
 
+// APIResponse represents the response from the trivia API.
 type APIResponse struct {
 	ResponseCode int        `json:"response_code"`
 	Results      []Question `json:"results"`
 }
 
+// Question represents a trivia question.
 type Question struct {
 	Category         string   `json:"category"`
 	Type             string   `json:"type"`
@@ -56,9 +61,10 @@ type Question struct {
 	Question         string   `json:"question"`
 	CorrectAnswer    string   `json:"correct_answer"`
 	IncorrectAnswers []string `json:"incorrect_answers"`
-	Points           int
+	Points           int      `json:"points"`
 }
 
+// mapDifficultyToPoints maps the difficulty level of each question to a corresponding point value.
 func mapDifficultyToPoints(questions []Question) {
 	for i, question := range questions {
 		switch question.Difficulty {
@@ -74,6 +80,11 @@ func mapDifficultyToPoints(questions []Question) {
 	}
 }
 
+// fetchTriviaQuestions fetches trivia questions from the external API.
+// The amount parameter specifies the number of questions to fetch.
+// The categoryId parameter specifies the category ID of the questions to fetch.
+// If categoryId is empty, questions from all categories will be fetched.
+// Returns the fetched questions and any error encountered.
 func fetchTriviaQuestions(amount string, categoryId string) ([]Question, error) {
 	params := url.Values{}
 	params.Add("amount", amount)
@@ -103,6 +114,11 @@ func fetchTriviaQuestions(amount string, categoryId string) ([]Question, error) 
 	return apiResponse.Results, nil
 }
 
+// triviaHandler handles the HTTP request for trivia questions.
+// It expects the "amount" and "category" query parameters.
+// If "amount" is not provided, it defaults to 3.
+// If "category" is not provided, questions from all categories will be fetched.
+// Returns the fetched questions as a JSON response.
 func triviaHandler(w http.ResponseWriter, req *http.Request) {
 	amount := req.URL.Query().Get("amount")
 	if amount == "" {
@@ -125,24 +141,15 @@ func triviaHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	port := os.Getenv("API_PORT")
+	port = fmt.Sprintf(":%s", port)
+	fmt.Println("Server running on port", port)
+
 	mux := http.NewServeMux()
-
-	mux.HandleFunc("/welcome", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Welcome to the welcome page!\n")
-	})
-
-	mux.HandleFunc("/home", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Welcome to the home page!\n")
-	})
-
-	mux.HandleFunc("/about", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Welcome to the about page!\n")
-	})
-
 	mux.HandleFunc("/trivia", triviaHandler)
 
 	n := negroni.Classic()
 	n.UseHandler(mux)
 
-	log.Fatal(http.ListenAndServe(":8080", n))
+	log.Fatal(http.ListenAndServe(port, n))
 }
